448ad63e28ab3bc79ab8990e9fd89d8819ac42dc
jawhrz <harizi.jawad123@gmail.com>
2025-05-22 19:42:50 +0200

    Update ResolutionFS.java

--- ResolutionFS.java
+++ ResolutionFS.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.control.Button;
@@ -21,10 +20,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Classe abstraite pour les algorithmes de résolution de labyrinthe de type
+ * parcours en largeur (BFS) ou en profondeur (DFS), héritée de {@link Resolution}.
+ * @author Jawad Harizi, @author Cherf Noam
+ */
 public abstract class ResolutionFS extends Resolution {
 
-	public abstract List<int[]> searchNeighbor(int[] position, MazeGenerator maze); 
+    /**
+     * Recherche les voisins atteignables depuis une position donnée dans le labyrinthe.
+     *
+     * @param position La position actuelle sous forme [ligne, colonne]
+     * @param maze Le labyrinthe dans lequel chercher les voisins
+     * @return Liste des positions voisines accessibles
+     */
+    public abstract List<int[]> searchNeighbor(int[] position, MazeGenerator maze); 
 
-	public abstract void highlightPathAnimation(MazeGenerator maze); 
-
+    /**
+     * Met en évidence de manière animée le chemin trouvé dans le labyrinthe.
+     *
+     * @param maze Le labyrinthe dans lequel afficher le chemin avec animation
+     */
+    public abstract void highlightPathAnimation(MazeGenerator maze); 
 }
